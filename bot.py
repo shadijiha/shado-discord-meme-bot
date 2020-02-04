512ee0f0daa11c72bf3fd94f8e874521d76f03cd
Shadi Jiha <shadijiha@gmail.com>
2020-02-04 11:39:46 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import os
+import requests
+from datetime import datetime
+
+import discord
+from discord.ext import commands
+
+
+#Discord Bot Token
+token = "Njc0MTE5NDkyOTYzNTMyODMw.Xjj9PQ.n4ysZom9eVJUqtn6Sf7Zkj5TqQY"
+#imgflip account
+acc_username = "jshado"
+acc_password = "q&#RqmycL=7Hu@6e"
+
+
+client = discord.Client()
+
+bot = commands.Bot(command_prefix='!')
+
+memes = requests.get('https://api.imgflip.com/get_memes').json()['data']['memes']
+
+@bot.command(name='meme')
+async def meme(ctx, meme_id: str, *args):
+  url = 'https://api.imgflip.com/caption_image'
+  template_id = 0
+  try:
+      # Check if ID supplied
+      template_id = int(meme_id)
+  except ValueError:
+      # If ID not supplied try to guess meme from name
+      for meme in memes:
+          if meme_id.casefold() in meme['name'].casefold():
+              template_id = meme['id']
+              break
+
+  params = dict(template_id=template_id, username=acc_username, password=acc_password)
+  for i, text in enumerate(args):
+      params["text"+str(i)] = text
+  res = requests.get(url, params=params)
+
+  data = res.json()
+
+    # Check for valid template_id
+  if data['success'] == False:
+    await ctx.send('Meme not found, check the ID with !meme_templates')
+    return
+  
+  print("\n\nTimestamp: " + str(datetime.now().strftime("%Y-%m-%d %H-%M-%S")) +
+        "\nMeme: " + str(data['data']['url']))
+    
+  await ctx.send(data['data']['url'])
+
+@bot.command(name='meme_templates')
+async def meme(ctx):
+  await ctx.send('https://api.imgflip.com/popular_meme_ids')
+
+bot.run(token)
