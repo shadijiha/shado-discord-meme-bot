4191306d8b7c253f6397da5b86043bd0fd2c6f0c
Shadi Jiha <shadijiha@gmail.com>
2020-02-04 13:53:52 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,11 +51,6 @@ async def meme(ctx, meme_id: str, *args):
     
   await ctx.send(data['data']['url'])
 
-@bot.command(name='templates')
-async def templates(ctx):
-  await ctx.replay("All memes here: https://shadijiha.github.io/shado-discord-meme-bot/index.html . Click on any meme to copy the bot command")
-
-
 @bot.command(name='meme_templates')
 async def meme(ctx):
   await ctx.send('https://api.imgflip.com/popular_meme_ids')
